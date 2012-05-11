cff51060b496eaff001fd4d87ac5ab5dd7eb8853
Joakim Soderberg <joakim.soderberg@gmail.com>
2012-05-11 11:40:41 +0200

    Added a CHECK_FUNCTIONS_EX cmake macro so that we can find functions in dll files also. Otherwise a lot of functions, such as getaddrinfo won't be found on windows.

---
+++ cmake/CheckFunctionExistsEx.c
@@ -0,0 +1,21 @@
+#ifdef CHECK_FUNCTION_EXISTS
+#ifdef __CLASSIC_C__
+int main(){
+  int ac;
+  char*av[];
+#else
+int main(int ac, char*av[]){
+#endif
+  void * p = &CHECK_FUNCTION_EXISTS;
+  if(ac > 1000)
+    {
+    return *av[0];
+    }
+  return 0;
+}
+
+#else  /* CHECK_FUNCTION_EXISTS */
+
+#  error "CHECK_FUNCTION_EXISTS has to specify the function"
+
+#endif /* CHECK_FUNCTION_EXISTS */
